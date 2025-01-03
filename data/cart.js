24d9564f6980eed59fba31c6201d3be809dda2c4
SanketGade07 <sjgade72@gmail.com>
2025-01-03 11:27:37 +0530

    organise the code using modules

--- data/cart.js
+++ data/cart.js
@@ -1,16 +1,47 @@
-const cart=[  ];
+export const cart=[  ];
 
-function updateCartTotalQuantity(){
-    let TotalQuantity=0;
-    cart.forEach((product)=>{
-        let quantity=product.quantity;
-        TotalQuantity+=quantity;
-    })
 
-    const cartQuantityDiv = document.querySelector('.js-cart-quantity');
-    cartQuantityDiv.innerHTML=TotalQuantity;
+
+export function isProductInCart(productId,quantity) {
+
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].id === productId) {
+      cart[i]['quantity']+=quantity;
+
+      return true;
+    }
+
+  }
+
+  return false
 
 }
+
+export function addToCart(button) {
+
+    button.addEventListener('click', () => {
+  
+    
+      const productId = button.dataset.productId;
+      addedMsgCheckmark(productId);
+      const quantity= addQuantityByDropdown(productId);
+  
+  
+      let productExist = isProductInCart(productId,quantity);
+      if (!productExist) {
+  
+        cart.push({
+          id: productId,
+          quantity:quantity
+        });
+  
+  
+  
+      }
+      updateCartTotalQuantity();
+      
+    })
+  }
 
 
 
